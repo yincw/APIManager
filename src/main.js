df30662f9edce4c6666ce939023416617ba3f4ab
yincw <120758164@qq.com>
2018-08-14 11:26:34 +0800

    修复 macOS 平台上退出应用后再次单击程序坞图标无法打开应用的 BUG

--- src/main.js
+++ src/main.js
@@ -80,9 +80,19 @@ async function createWindow () {
 
 }
 
+// 当所有的窗口被关闭后退出应用
 app.on('window-all-closed', () => {
+  // 对于OS X系统，应用和相应的菜单栏会一直激活直到用户通过Cmd + Q显式退出
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // 对于OS X系统，当dock图标被点击后会重新创建一个app窗口，并且不会有其他
+  // 窗口打开
+  if (win === null) {
+    createWindow();
   }
 });
 
